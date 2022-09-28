b4424f1b6b8ea11d24a22aa58396861780d11f23
Davidhoechtl <davidhoechtl.job@gmail.com>
2022-09-28 10:32:14 +0200

    IoC Container eingebaut

---
+++ VsProject/MonsterTradingCardGame_Hoechtl/ContainerBootstrapper.cs
@@ -0,0 +1,48 @@
+﻿using Autofac;
+using MTCG.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTradingCardGame_Hoechtl
+{
+    internal class ContainerBootstrapper
+    {
+        private readonly ContainerBuilder builder;
+
+        public ContainerBootstrapper(ContainerBuilder builder)
+        {
+            this.builder = builder;
+        }
+
+        public IContainer Build()
+        {
+            return builder.Build();
+        }
+
+        public void Register()
+        {
+            RegisterInfrastructure();
+            RegisterRepository();
+            RegisterService();
+        }
+
+        private void RegisterInfrastructure()
+        {
+            builder.RegisterType(
+                typeof(PackageFactory)
+            )
+            .SingleInstance();
+        }
+
+        private void RegisterRepository()
+        {
+        }
+
+        private void RegisterService()
+        {
+        }
+    }
+}
--- VsProject/MonsterTradingCardGame_Hoechtl/Program.cs
+++ VsProject/MonsterTradingCardGame_Hoechtl/Program.cs
@@ -1,16 +1,23 @@
-﻿using MTCG.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Autofac;
+using MTCG.Models;
 
 namespace MonsterTradingCardGame_Hoechtl
 {
     class Program
     {
+        private static IContainer container;
         public static void Main(string[] args)
         {
+            // Initialisieren des Container Builders
+            ContainerBuilder containerBuilder = new();
+            ContainerBootstrapper bootstrapper = new(containerBuilder);
+
+            // Registrierung der jeweiligen Instancen
+            bootstrapper.Register();
+
+            // build des Containers
+            container = bootstrapper.Build();
+
             User testUser = new User()
             {
                 Credentials = new UserCredentials("Test", "Test")
