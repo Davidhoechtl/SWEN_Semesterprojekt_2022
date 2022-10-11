13e319cc9915b830ba9512a027728fbb8c712856
Davidhoechtl <davidhoechtl.job@gmail.com>
2022-10-11 14:05:51 +0200

    Weitere UnitTests für die Battle Logik hinzugefügt

--- VsProject/MTCG.Tests/BattleLogicTests.cs
+++ VsProject/MTCG.Tests/BattleLogicTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MTCG.Logic.Models.Spells;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -71,6 +72,71 @@ namespace MTCG.Tests
 
             Assert.That(elve == result);
             Assert.That(elve == result2);
+        }
+
+        [Test]
+        public void Test_FireSpellVsWaterSpell()
+        {
+            FireSpell fireSpell = new() { Damage = 10, ElementTyp = ElementTyp.Fire };
+            WaterSpell waterSpell = new() { Damage = 10, ElementTyp = ElementTyp.Water };
+
+            Card result = fireSpell.BattleAgainst(waterSpell);
+            Card result2 = waterSpell.BattleAgainst(fireSpell);
+
+            Assert.That(waterSpell == result);
+            Assert.That(waterSpell == result2);
+        }
+
+        [Test]
+        public void Test_FireSpellVsRegularSpell()
+        {
+            FireSpell fireSpell = new() { Damage = 10, ElementTyp = ElementTyp.Fire };
+            RegularSpell regularSpell = new() { Damage = 10, ElementTyp = ElementTyp.Normal };
+
+            Card result = fireSpell.BattleAgainst(regularSpell);
+            Card result2 = regularSpell.BattleAgainst(fireSpell);
+
+            Assert.That(fireSpell == result);
+            Assert.That(fireSpell == result2);
+        }
+
+        [Test]
+        public void Test_WaterSpellVsRegularSpell()
+        {
+            WaterSpell waterSpell = new() { Damage = 10, ElementTyp = ElementTyp.Water };
+            RegularSpell regularSpell = new() { Damage = 10, ElementTyp = ElementTyp.Normal };
+
+            Card result = waterSpell.BattleAgainst(regularSpell);
+            Card result2 = regularSpell.BattleAgainst(waterSpell);
+
+            Assert.That(regularSpell == result);
+            Assert.That(regularSpell == result2);
+        }
+
+        [Test]
+        public void Test_RegularSpellVsRegularSpell()
+        {
+            RegularSpell regularSpell = new() { Damage = 10, ElementTyp = ElementTyp.Normal };
+            RegularSpell regularSpell2 = new() { Damage = 10, ElementTyp = ElementTyp.Normal };
+
+            Card result = regularSpell.BattleAgainst(regularSpell2);
+            Card result2 = regularSpell2.BattleAgainst(regularSpell);
+
+            Assert.IsNull(result);
+            Assert.IsNull(result2);
+        }
+
+        [Test]
+        public void Test_FireSpellVsWaterMonster()
+        {
+            FireSpell fireSpell = new() { Damage = 10, ElementTyp = ElementTyp.Fire };
+            Goblin waterGoblin = new() { Damage = 10, ElementTyp = ElementTyp.Water };
+
+            Card result = fireSpell.BattleAgainst(waterGoblin);
+            Card result2 = waterGoblin.BattleAgainst(fireSpell);
+
+            Assert.That(waterGoblin == result);
+            Assert.That(waterGoblin == result2);
         }
     }
 }
