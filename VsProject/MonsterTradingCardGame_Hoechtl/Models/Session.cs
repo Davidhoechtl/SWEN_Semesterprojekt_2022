0b3c6bae64122aa1db73d94d8c0a2423d9066834
Davidhoechtl <davidhoechtl.job@gmail.com>
2022-11-02 09:52:27 +0100

    Server Models hinzugefügt

---
+++ VsProject/MonsterTradingCardGame_Hoechtl/Models/Session.cs
@@ -0,0 +1,16 @@
+﻿using MTCG.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTradingCardGame_Hoechtl.Models
+{
+    internal class Session
+    {
+        public string SessionToken { get; }
+        public Role Role { get; }
+        public User User { get; }
+    }
+}
